66583baba338cbec2d069fb851b11f47b89531f7
xuv <18742123+xuvatilavv@users.noreply.github.com>
2022-08-14 20:20:34 +0000

    Add conversation JSON validation
     - Validate Dialog scenes
     - Validate MultipleChoice scenes

--- managers/ConversationManager.gd
+++ managers/ConversationManager.gd
@@ -24,7 +24,8 @@ func _init():
 	# TODO error checking and stuff
 	_conversations = JSON.parse(json_raw).result
 	
-	
+	if OS.is_debug_build():
+		validate()
 
 
 func show(conversation_name: String):
@@ -44,3 +45,98 @@ func show(conversation_name: String):
 			var _err = get_tree().change_scene_to(_KnockCodeScene)
 		"MultipleChoice":
 			var _err = get_tree().change_scene_to(_MultipleChoiceScene)
+
+
+func validate():
+	print_debug("Validating conversations...")
+	for id in _conversations.keys():
+		if typeof(_conversations.get(id)) != TYPE_DICTIONARY:
+			push_error("Convo '%s': Not a Dictionary." % id)
+			continue
+
+		var dict: Dictionary = _conversations.get(id)
+		var errors = []
+		match dict.get("type"):
+			null:
+				errors.append("Missing 'type'.")
+			"Dialog":
+				errors += validate_Dialog(dict)
+			"MultipleChoice":
+				errors += validate_MultipleChoice(dict)
+			_:
+				errors.append("Unknown type: '%s'" % dict.get("type"))
+		
+		for err in errors:
+			push_error("Convo '%s': %s" % [id, err])
+
+
+func validate_strings(strings: Array) -> Array:
+	var errors = []
+	
+	for string in strings:
+		# Doesn't account for keys that match their translation but that should
+		# rarely or never happen. Also only checks the current locale.
+		if tr(string) == string:
+			errors.append(string)
+
+	return errors
+
+
+func validate_Dialog(dict: Dictionary) -> Array:
+	var errors = []
+
+	var strings = dict.get("strings")
+	if strings == null:
+		errors.append("Missing 'strings'.")
+	elif typeof(strings) != TYPE_ARRAY:
+		errors.append("Strings is not an Array.")
+	else:
+		var bad_strings = validate_strings(strings)
+		if not bad_strings.empty():
+			errors.append("Invalid String IDs: %s" % bad_strings)
+	
+	# TODO validate image is specified and exists
+	
+	var next = dict.get("next")
+	if next == null:
+		errors.append("Missing 'next'.")
+	elif _conversations.get(next) == null:
+		errors.append("Invalid next convo: '%s'" % next)
+	
+	return errors
+
+
+func validate_MultipleChoice(dict: Dictionary) -> Array:
+	var errors = []
+
+	var query = dict.get("query")
+	if query == null:
+		errors.append("Missing 'query'.")
+	elif typeof(query) != TYPE_STRING:
+		errors.append("Query is not a String.")
+	else:
+		var bad_strings = validate_strings([query])
+		if not bad_strings.empty():
+			errors.append("Query is invalid ID: '%s'" % query)
+	
+	var responses = dict.get("responses")
+	if responses == null:
+		errors.append("Missing 'responses'.")
+	elif typeof(responses) != TYPE_ARRAY:
+		errors.append("Responses is not an Array.")
+	else:
+		var bad_strings = validate_strings(responses)
+		if not bad_strings.empty():
+			errors.append("Invalid String IDs: %s" % bad_strings)
+	
+	var branches = dict.get("branches")
+	if branches == null:
+		errors.append("Missing 'branches'.")
+	elif typeof(branches) != TYPE_ARRAY:
+		errors.append("Branches is not an Array.")
+	else:
+		for br in branches:
+			if _conversations.get(br) == null:
+				errors.append("Invalid branch target: %s" % br)
+	
+	return errors
